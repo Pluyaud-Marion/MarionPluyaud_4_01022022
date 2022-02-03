2589def84f95a5f1267c542709587215ba44079a
Marion Pluyaud <marion070189@hotmail.fr>
2022-02-03 16:20:39 +0100

    résolution des beugs, initialisation eslint + finalisation de la vérification du formulaire et de l'affichage du message de confirmation

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -1,107 +1,199 @@
+/* eslint-disable no-undef */
+
+// eslint-disable-next-line no-unused-vars
 function editNav() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
-  } else {
-    x.className = "topnav";
-  }
+	var x = document.getElementById("myTopnav");
+	if (x.className === "topnav") {
+		x.className += " responsive";
+	} else {
+		x.className = "topnav";
+	}
 }
 
 // DOM Elements
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
-const formData = document.querySelectorAll(".formData");
-const closeButton = document.querySelector(".close"); // croix pour fermer la modale
+//const formData = document.querySelectorAll(".formData");
+// croix pour fermer la modale
+const closeButton = document.querySelector(".close");
+// bouton formulaire
 const buttonSendForm = document.querySelector(".btn-submit");
-
-
-// launch modal event
-modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
-
-// launch modal form
-function launchModal() {
-  modalbg.style.display = "block";
-}
-
-//au click sur la croix appel de la fonction closeModal
-closeButton.addEventListener("click", closeModal);
-
-//fonction pour fermer la modale = passe à display none
-function closeModal() {
-  modalbg.style.display = "none";
-}
-
-const regexName = /^[a-zA-ZÀ-ÿ\s_-]{2,60}$/;
-const regexEmail = /^[^@\s]{2,30}@[^@\s]{2,30}\.[^@\s]{2,5}$/;
-const regexNumber = /^[0-9]+$/; 
-
-
+// champs et checkboxs
 const firstnameField = document.getElementById("first");
 const lastnameField = document.getElementById("last");
 const emailField = document.getElementById("email");
 const birthdateField = document.getElementById("birthdate");
 const numberTournamentField = document.getElementById("quantity");
-const checkBox = document.getElementById("checkbox1"); 
-
-//selectionne tous les boutons radios
+const checkBox = document.getElementById("checkbox1");
+//tous les boutons radios
 const buttonsRadio = document.querySelectorAll(".checkbox-location");
 
+// Regex
+const regexName = /^[a-zA-ZÀ-ÿ\s_-]{2,60}$/;
+const regexEmail = /^[^@\s]{2,30}@[^@\s]{2,30}\.[^@\s]{2,5}$/;
+const regexNumber = /^[0-9]+$/;
 
+// launch modal event
+modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+
+// au click sur la croix appel de la fonction closeModal
+closeButton.addEventListener("click", closeModal);
+
+// au click sur valider le formulaire appel de fonction verifyForm et submitForm
 buttonSendForm.addEventListener("click", event => {
-  event.preventDefault();
-  submitForm();
-  
-})
+	event.preventDefault();
+	verifyForm();
+	submitForm();
+});
 
-
-
-function submitForm() {
- 
-  if (firstnameField.value === "") {
-    document.getElementById("error-firstname").innerHTML = "Ce champ doit être rempli"
-  } else if (!regexName.test(firstnameField.value)) {
-    document.getElementById("error-firstname").innerHTML = "Format incorrect, vous devez renseigner au moins 2 caractères"
-  } 
-
-  if (lastnameField.value === "") {
-    document.getElementById("error-lastname").innerHTML = "Ce champ doit être rempli"
-  } else if (!regexName.test(lastnameField.value)) {
-    document.getElementById("error-lastname").innerHTML = "Format incorrect, vous devez renseigner au moins 2 caractères"
-  } 
-
-  if (emailField.value === "") {
-    document.getElementById("error-email").innerHTML = "Ce champ doit être rempli"
-  } else if(!regexEmail.test(emailField.value)) {
-    document.getElementById("error-email").innerHTML = "Votre adresse mail n'est pas valide"
-  } 
-
-  if(birthdateField.value === "") {
-    document.getElementById("error-birthdate").innerHTML = "Vous devez renseigner votre date de naissance"
-  }
-
-  if (numberTournamentField.value === "") {
-    document.getElementById("error-quantity").innerHTML = "Ce champ doit être rempli"
-  } else if(!regexNumber.test(numberTournamentField.value)){
-    document.getElementById("error-quantity").innerHTML = "Seuls les chiffres sont acceptés"
-  } 
-
-  //boucle sur tous les boutons radio
-  for (button of buttonsRadio){
-    if(button.checked === false){ // si renvoie false = non coché -> affiche message d'erreur
-      document.getElementById("error-location").innerHTML = "Vous devez choisir une ville"
-    } else {
-      document.getElementById("error-location").innerHTML = ""
-    }
-  }
-
-  if(checkBox.checked === false){ // retourne true si coché / false si décoché
-    document.getElementById("error-checkbox").innerHTML = "Vous devez cocher cette case"
-  }
-
-
-  // document.querySelector(".modal").innerHTML = "Merci, votre réservation a bien été reçue!"
-  // buttonSendForm.value= "J'ai compris"
-
-  
+// launch modal form
+function launchModal() {
+	modalbg.style.display = "block";
 }
+
+// fonction pour fermer la modale = passe à display none
+function closeModal() {
+	modalbg.style.display = "none";
+	window.location.reload();
+}
+
+/*
+fonction qui check tous les champs => si tous les champs sont ok, envoi le formulaire et fait afficher message confirmation (+ gestion fermeture de la confirmation au click sur bouton)
+*/
+function submitForm() {
+
+	for (button of buttonsRadio) {
+		
+		// if ((firstnameField.value != "") && regexName.test(firstnameField.value) && (lastnameField.value != "") && regexName.test(lastnameField.value) && (emailField.value != "") && regexEmail.test(emailField.value) && (birthdateField.value != "") && (numberTournamentField.value != "") && regexNumber.test(numberTournamentField.value) && checkBox.checked === true && button.checked === true) {
+
+		//Si tous les champs sont ok
+		if (checkFirstname() && checkLastname() && checkEmail() && checkBirthdate() && checkNumberTournament() && checkCheckBox() && checkCity()) {
+			//on remplace le contenu de la div .modal par le texte
+			document.querySelector(".modal").innerHTML = "Merci, votre réservation a bien été reçue!";
+			// on remplace la valeur du bouton "c'est parti" par "j'ai compris"
+			buttonSendForm.value = "J'ai compris";
+			//au click sur le bouton on appelle la fonction pour fermer la modale
+			buttonSendForm.addEventListener("click", closeModal);
+		}
+	}
+}
+
+/*
+fonction qui check le champ firstname et affiche les messages d'erreurs si besoin
+*/
+function checkFirstname() {
+	if (firstnameField.value === "") {
+		document.getElementById("error-firstname").innerHTML = "Ce champ doit être rempli";
+	} else if (!regexName.test(firstnameField.value)) {
+		document.getElementById("error-firstname").innerHTML = "Format incorrect, vous devez renseigner au moins 2 caractères";
+	} else {
+		document.getElementById("error-firstname").innerHTML = "";
+		return true;
+	}
+}
+
+/*
+fonction qui check le champ lastname et affiche les messages d'erreurs si besoin
+*/
+function checkLastname() {
+	if (lastnameField.value === "") {
+		document.getElementById("error-lastname").innerHTML = "Ce champ doit être rempli";
+	} else if (!regexName.test(lastnameField.value)) {
+		document.getElementById("error-lastname").innerHTML = "Format incorrect, vous devez renseigner au moins 2 caractères";
+	} else {
+		document.getElementById("error-lastname").innerHTML = "";
+		return true;
+	}
+}
+
+/*
+fonction qui check le champ email et affiche les messages d'erreurs si besoin
+*/
+function checkEmail() {
+	if (emailField.value === "") {
+		document.getElementById("error-email").innerHTML = "Ce champ doit être rempli";
+	} else if (!regexEmail.test(emailField.value)) {
+		document.getElementById("error-email").innerHTML = "Votre adresse mail n'est pas valide";
+	} else {
+		document.getElementById("error-email").innerHTML = "";
+		return true;
+	}
+}
+
+/*
+fonction qui check le champ birthdate et affiche les messages d'erreurs si besoin
+*/
+function checkBirthdate() {
+	if (birthdateField.value === "") {
+		document.getElementById("error-birthdate").innerHTML = "Vous devez renseigner votre date de naissance";
+	} else {
+		document.getElementById("error-birthdate").innerHTML = "";
+		return true;
+	}
+}
+
+/*
+fonction qui check le champ nombre de tournoi et affiche les messages d'erreurs si besoin
+*/
+function checkNumberTournament() {
+	if (numberTournamentField.value === "") {
+		document.getElementById("error-quantity").innerHTML = "Ce champ doit être rempli";
+	} else if (!regexNumber.test(numberTournamentField.value)) {
+		document.getElementById("error-quantity").innerHTML = "Seuls les chiffres sont acceptés";
+	} else {
+		document.getElementById("error-quantity").innerHTML = "";
+		return true;
+	}
+}
+
+
+/*
+fonction qui check si un bouton radio est coché et affiche les messages d'erreurs si besoin
+*/
+function checkCity() {
+
+	for (button of buttonsRadio) {
+
+		if (button.checked) {
+			document.getElementById("error-location").innerHTML = "";
+			return true;
+	
+		} else {
+			document.getElementById("error-location").innerHTML = "Vous devez choisir une ville";
+		}
+	}
+
+}
+
+/*
+fonction qui check si le checkbox est coché et affiche les messages d'erreurs si besoin
+*/
+function checkCheckBox() {
+	if (checkBox.checked === false) { // retourne true si coché / false si décoché
+		document.getElementById("error-checkbox").innerHTML = "Vous devez cocher cette case";
+	} else {
+		document.getElementById("error-checkbox").innerHTML = "";
+		return true;
+	
+	}
+}
+
+/*
+fonction qui appelle toutes les fonctions pour tester les champs et afficher les messages d'erreur
+*/
+function verifyForm() {
+	const arrayButtonsRadio = Array.from(buttonsRadio); // transforme objet en array
+	arrayButtonsRadio.some(checkCity); // Vérifie dans l'array des boutons radio si un élément retourne true
+	checkFirstname();
+	checkLastname();
+	checkEmail();
+	checkBirthdate();
+	checkNumberTournament();
+	checkCity();
+	checkCheckBox();
+}
+
+
+
+
 
